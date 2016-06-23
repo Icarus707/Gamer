fb4906a8e1e0a11850500eced23d842db1a011ee
Jay Rosenthal <kjrosenthal90@yahoo.com>
2016-06-22 21:47:48 -0500

    JS
    The struggle is real. Im done for the night

---
+++ gamer.js
@@ -0,0 +1,36 @@
+function submitItem()
+
+{
+
+	var disp = ["Disp1", "Disp2", "Disp3", "Disp4", "Disp5", "Disp16", "Disp7", "Disp8", "Disp9", "Disp10", "Disp11", "Disp12"];
+	var disp = new Array();
+
+
+		disp[0] = document.getElementById("disp1").value;
+		disp[1] = document.getElementById("disp2").value;
+		disp[2] = document.getElementById("disp3").value;
+		disp[3] = document.getElementById("disp4").value;
+		disp[4] = document.getElementById("disp5").value;
+		disp[5] = document.getElementById("disp6").value;
+		disp[6] = document.getElementById("disp7").value;
+		disp[7] = document.getElementById("disp8").value;
+		disp[8] = document.getElementById("disp9").value;
+		disp[9] = document.getElementById("disp10").value;
+		disp[10] = document.getElementById("disp11").value;
+		disp[11] = document.getElementById("disp12").value;
+
+
+				console.log(disp[0]);
+				console.log(disp[1]);
+				console.log(disp[2]);
+				console.log(disp[3]);
+				console.log(disp[4]);
+				console.log(disp[5]);
+				console.log(disp[6]);
+				console.log(disp[7]);
+				console.log(disp[8]);
+				console.log(disp[9]);
+				console.log(disp[10]);
+				console.log(disp[11]);
+
+			}
